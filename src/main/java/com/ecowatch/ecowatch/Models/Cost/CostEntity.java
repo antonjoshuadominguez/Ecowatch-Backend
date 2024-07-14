199338ad057276cf2b009f7ab179d7564020aef8
antonjoshuadominguez <antonjoshua.dominguez@cit.edu>
2024-07-14 23:12:29 +0800

    Added cost schema

---
+++ src/main/java/com/ecowatch/ecowatch/Models/Cost/CostEntity.java
@@ -0,0 +1,21 @@
+package com.ecowatch.ecowatch.Models.Cost;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Table(name = "costs", schema = "public")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CostEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String type;
+    private String currency;
+    private double amount;
+    private LocalDate date_added;
+}
